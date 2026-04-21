9315cdf10770cb434fd66a84e9e284fb470d11de
nietocastellanos1 <j.nieto.castellanos@fz-juelich.de>
2026-04-21 11:24:18 +0200

    parallel transport implemented

--- tests/test_latticeConv.py
+++ tests/test_latticeConv.py
@@ -26,8 +26,8 @@ class TestLCNN():
         #gauge_conf = [Re(U0),Re(U1),Im(U0),Im(U1)]
         #    gconf is float64 and tvectors complex128. The last entry has the indices of the confs.
         self.first_batch = next(iter(dataloader))
-        confID = 0 
-        self.data = self.first_batch[confID].to(var.DEVICE) #[Re(U0),Re(U1),Im(U0),Im(U1), Re(W),Im(W)], where W'(x) = Omega(x) W(x) Omega^+(x)
+        self.confID = 0 
+        self.data = self.first_batch[self.confID].to(var.DEVICE) #[Re(U0),Re(U1),Im(U0),Im(U1), Re(W),Im(W)], where W'(x) = Omega(x) W(x) Omega^+(x)
         print(len(self.data))
         self.conf = self.data[:,0:4]
         self.w = self.data[:,4:]
@@ -35,10 +35,18 @@ class TestLCNN():
 
     def check_parallel_transport(self):
         k, mu, orientation = 1, 0, 1
-        wkxmu = torch.roll(self.w,shifts=k*orientation,dims=mu)   #w[(t,x)+k * orientation *hat{mu}]
+        wkxmu = torch.roll(self.w,shifts=k*orientation,dims=mu+2)   #w[(t,x)-k * orientation *hat{mu}]
 
         #assert self.w[0,0,0] == wkxmu[0,k*mu,0], "W(0,0) = {0} and W(k mu,0) = {1}".format(self.w[0,0,0],wkxmu[0,k*mu,0])
-        print("W(0,0)={0}, W(-kmu,0)={1}".format(self.w[:,0,0],wkxmu[:,-k*mu,0]))
-        print("W(0,0)={0}, W(kmu,0)={1}".format(self.w[:,0,0],wkxmu[:,k*mu,0]))
-        print("W(0,0)={0}, W(0,-kmu)={1}".format(self.w[:,0,0],wkxmu[:,0,-k*mu]))
-        print("W(0,0)={0}, W(0,kmu)={1}".format(self.w[:,0,0],wkxmu[:,0,k*mu]))
+        print("W(-1,0)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,-1,0],wkxmu[self.confID,0,0,0]))
+        print("W(-1,0)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,-1,0],wkxmu[self.confID,0,0,0]))
+        print("W(-1,0)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,-1,0],wkxmu[self.confID,0,0,0]))
+        print("W(-1,0)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,-1,0],wkxmu[self.confID,0,0,0]))
+        print("")
+        k, mu, orientation = 1, 1, 1
+        wkxmu = torch.roll(self.w,shifts=k*orientation,dims=mu+2)   #w[(t,x)-k * orientation *hat{mu}]
+        print("W(0,-1)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,0,-1],wkxmu[self.confID,0,0,0]))
+        print("W(0,-1)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,0,-1],wkxmu[self.confID,0,0,0]))
+        print("W(0,-1)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,0,-1],wkxmu[self.confID,0,0,0]))
+        print("W(0,-1)={0}, Wkxmu(0,0)={1}".format(self.w[self.confID,0,0,-1],wkxmu[self.confID,0,0,0]))
+        return wkxmu
